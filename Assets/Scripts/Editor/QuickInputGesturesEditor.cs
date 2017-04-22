e9410cc0648c69b9ac30fe524f6367537191811a
Spencer <spencer@poonwoo.com>
2017-04-22 16:57:55 -0400

    -Touch controls implemented

---
+++ Assets/Scripts/Editor/QuickInputGesturesEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Events;
+using UnityEditorInternal;
+using System.Linq;
+
+[CustomEditor(typeof(QuickInputGestures))]
+public class QuickInputGesturesEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		QuickInputGestures myTarget = (QuickInputGestures)target;
+
+		if (myTarget.events == null) myTarget.events = new UnityEvent[4];
+
+		myTarget.threshold = EditorGUILayout.FloatField("Threshold", myTarget.threshold);
+
+		for(int i = 0; i < 4; i++) {
+			EditorGUILayout.LabelField(((QuickInputGestures.Direction)i).ToString());
+			SerializedProperty sProp = serializedObject.FindProperty("events").GetArrayElementAtIndex(i);
+			EditorGUIUtility.LookLikeControls();
+			EditorGUILayout.PropertyField(sProp,true);
+		}
+
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
---
+++ Assets/Scripts/QuickInputGestures.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+///<summary>
+/// An easy way to hookup key presses to UnityEvents.
+/// Used a custom inspector to use in editor.
+/// Would look and function better if I had more time.
+///</summary>
+
+public class QuickInputGestures : MonoBehaviour {
+	public enum Direction {
+		Up,
+		Down,
+		Right,
+		Left
+	}
+	public UnityEvent[] events;
+	public float threshold;
+
+	private Vector2 touchStart;
+
+	private void Start() {
+		Messenger.AddListener("DisableInput",DisableInput);
+		Messenger.AddListener("EnableInput",EnableInput);
+
+	}
+
+	private void DisableInput() {
+		enabled = false;
+	}
+
+	private void EnableInput() {
+		enabled = true;
+	}
+
+	private void Update() {
+		if(Input.touchCount > 0) {
+			if(Input.GetTouch(0).phase == TouchPhase.Began) {
+				touchStart = Input.GetTouch(0).position;
+			} else if(Input.GetTouch(0).phase == TouchPhase.Ended) {
+				Vector2 touchEnd = Input.GetTouch(0).position;
+
+				if ( Mathf.Abs(touchStart.y - touchEnd.y) > threshold) {
+					if(touchStart.y < touchEnd.y)
+						events[(int)Direction.Up].Invoke();
+					else
+						events[(int)Direction.Down].Invoke();
+				}
+
+				if ( Mathf.Abs(touchStart.x - touchEnd.x) > threshold) {
+					if(touchStart.x < touchEnd.x)
+						events[(int)Direction.Right].Invoke();
+					else
+						events[(int)Direction.Left].Invoke();
+				}
+			}
+		}
+	}
+}
