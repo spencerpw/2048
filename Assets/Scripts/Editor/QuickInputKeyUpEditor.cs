6d225e8bf5e7ad33f24d67d94797db8addb9a1b9
Spencer <spencer@poonwoo.com>
2017-04-22 13:51:47 -0400

    -Cool way to quickly hookup keys to public functions

---
+++ Assets/Scripts/Editor/QuickInputKeyUpEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Events;
+using UnityEditorInternal;
+using System.Linq;
+
+[CustomEditor(typeof(QuickInputKeyUp))]
+public class QuickInputKeysEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		QuickInputKeyUp myTarget = (QuickInputKeyUp)target;
+		List<UnityEvent> myEvents;
+
+		if (myTarget.keyCodes == null) myTarget.keyCodes = new List<KeyCode>();
+		if (myTarget.events == null) myTarget.events = new UnityEvent[1];
+
+		myEvents = myTarget.events.ToList();
+
+		for(int i = 0; i < myTarget.keyCodes.Count; i++) {
+			myTarget.keyCodes[i] = (KeyCode)EditorGUILayout.EnumPopup("KeyCode",myTarget.keyCodes[i]);
+
+			if(myTarget.keyCodes[i] != KeyCode.None) {
+				SerializedProperty sProp = serializedObject.FindProperty("events").GetArrayElementAtIndex(i);
+				EditorGUIUtility.LookLikeControls();
+				EditorGUILayout.PropertyField(sProp,true);
+
+			} else {
+				myTarget.keyCodes.RemoveAt(i);
+				myEvents.RemoveAt(i);
+			}
+		}
+
+		if(GUILayout.Button("Add")) {
+			myTarget.keyCodes.Add(KeyCode.A);
+			myEvents.Add(new UnityEvent());
+			myTarget.events = myEvents.ToArray();
+		}
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
---
+++ Assets/Scripts/QuickInputKeyUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+///<summary>
+/// An easy way to hookup key presses to UnityEvents.
+/// Used a custom inspector to use in editor.
+/// Would look and function better if I had more time.
+///</summary>
+
+public class QuickInputKeyUp : MonoBehaviour {
+	public List<KeyCode> keyCodes;
+	public UnityEvent[] events;
+
+	private void Update() {
+		for(int i = 0; i < keyCodes.Count; i++) {
+			if(Input.GetKeyUp(keyCodes[i])) {
+				events[i].Invoke();
+			}
+		}
+	}
+}
