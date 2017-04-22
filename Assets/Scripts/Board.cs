aea1b30c666c0670c6c7f162804c56fa10405682
Spencer <spencer@poonwoo.com>
2017-04-22 12:52:18 -0400

    -Game starts with two tiles randomly positioned -Dropdown item to test tile spawning -Tiles colour by number

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class Board : MonoBehaviour {
 	[SerializeField] private int rows;
 	[SerializeField] private int columns;
 	[SerializeField] private float tilePadding;
 	[SerializeField] private Image tileBackground;
+	[SerializeField] private Transform backgroundRoot;
+	[SerializeField] private Transform tileRoot;
+	[SerializeField] private Tile tilePrefab;
 
-	private RectTransform rectTransform;
+	public RectTransform rectTransform;
+
 	private Tile[,] tiles;
 	private Vector2 tileSize;
 
 	private void Awake() {
-		rectTransform = GetComponent<RectTransform>();
 		tiles = new Tile[rows,columns];
 
 		CalculateSizes();
@@ -22,6 +26,10 @@ public class Board : MonoBehaviour {
 
 	private void Start() {
 		DrawEmptyTiles();
+
+		//Begin game with two tiles
+		SpawnTile();
+		SpawnTile();
 	}
 
 	private void CalculateSizes() {
@@ -33,22 +41,67 @@ public class Board : MonoBehaviour {
 	private void DrawEmptyTiles() {
 		for(int i = 0; i < rows; i++) {
 			for(int j = 0; j < columns; j++) {
-				Image newBG = GameObject.Instantiate(tileBackground, transform);
-				Vector3 pos = Vector3.zero;
-
-				pos.x = j * tileSize.x + (j+1) * (tilePadding);
-				pos.y = i * tileSize.y + (i+1) * (tilePadding);
-
+				Image newBG = GameObject.Instantiate(tileBackground, backgroundRoot);
 				newBG.name = string.Format("{0},{1}",j,i);
 				newBG.rectTransform.sizeDelta = tileSize;
-				newBG.rectTransform.localPosition = pos;
-			
+				newBG.rectTransform.localPosition = GetBoardPosition(i,j);
 
 			}
 		}
 	}
 
+	private Vector3 GetBoardPosition(int row, int col) {
+		Vector3 pos = Vector3.zero;
+
+		pos.x = col * tileSize.x + (col+1) * (tilePadding);
+		pos.y = row * tileSize.y + (row+1) * (tilePadding);
+
+		return pos;
+	}
+
+	public void SpawnTile() {
+		//First check if board is full in a fast to write slow as hell crappy way
+		if(FindObjectsOfType<Tile>().Count() == rows * columns) {
+			Debug.LogWarning("Board::SpawnTile A tile could not be spawned. The board is full");
+			return;
+		}
+
+		//Find an empty space
+		//pick a random starting point
+		//if it's occupied iterate through until reaches beginning
+		//O(n*m) worst case where 
+		int r = Random.Range(0,rows);
+		int c = Random.Range(0,columns);
+
+		if(tiles[r,c] != null) {
+			for(int i = r+1; i != r; i++) {
+				if(i >= rows) i = 0;
+
+				for(int j = 0; j < columns; j++) {
+					if(tiles[i,j] == null) {
+						r = i;
+						c = j;
+						break;
+					}
+				}
+
+				if(tiles[r,c] == null) break;
+			}
+		}
+
+
+		Tile newTile = GameObject.Instantiate(tilePrefab, tileRoot);
+		newTile.rectTransform.sizeDelta = tileSize;
+		newTile.transform.localPosition = GetBoardPosition(r,c);
+		tiles[r,c] = newTile;
+	}
+
 	#if UNITY_EDITOR
+
+	[ContextMenu("Spawn Tile")]
+	public void TestSpawn() {
+		SpawnTile();
+	}
 
 	[ContextMenu("Redraw")]
 	public void RedrawBoard() {
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Tile : MonoBehaviour {
+	[SerializeField] private TextMeshProUGUI numberText;
+	[SerializeField] private Image image;
+	[SerializeField] private Color startColour;
+	[SerializeField] private Color endColour;
 
-	// Use this for initialization
-	void Start () {
-		
+	public RectTransform rectTransform;
+
+	private int number;
+
+	public int Number {
+		get {
+			return number;
+		}
+		set {
+			number = value;
+			numberText.text = name = number.ToString();
+			SetColour();
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void Awake() {
+		Number = Random.Range(0,2) == 0 ? 2 : 4;
+	}
+
+	public void SetColour() {
+		float power = Mathf.Log(Number) / Mathf.Log(2);
+		image.color = Color.Lerp(startColour,endColour, (power-1)/11f);
+
 	}
 }
